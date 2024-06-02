1d5dca3f37ad8358865abdb2d967cb341cb660e5
Aryan Tikarya <aryan.tikarya@dojima.network>
2024-06-02 19:58:16 +0530

    add approve script and fix deploy

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,23 +1,23 @@
 require("hardhat-deploy")
 require("hardhat-deploy-ethers")
 require("dotenv").config({ path: './shared/config/.env' })
+const {ethers} = require("hardhat")
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.provider)
-
 module.exports = async ({ deployments }) => {
     const {deploy} = deployments;
     console.log("Wallet Ethereum Address:", wallet.address)
-    // const labCoinContract = await deploy("Labcoin", {
-    //     from: wallet.address,
-    //     args: [wallet.address],
-    //     log: true,
-    // })
 
-    const researchContract = await deploy("Research", {
-        from: wallet.address,
-        args: ["0xb8c468321368fe8b367914d656b724e31CEe2708"],
-        log: true,
-    })
+    const labCoinContract = await ethers.getContractFactory("Labcoin");
+    const labCoin = await labCoinContract.connect(wallet)
+    const labcoin = await labCoin.deploy(wallet.address);
+    await labcoin.waitForDeployment()
+    console.log("Labcoin Contract Address:", labcoin.target)
 
+    const researchContract = await ethers.getContractFactory("Research");
+    const res = await researchContract.connect(wallet)
+    const research = await res.deploy("0xb8c468321368fe8b367914d656b724e31CEe2708");
+    await research.waitForDeployment()
+    console.log("Research Contract Address:", research.target)
 }
---
+++ tasks/approve.js
@@ -0,0 +1,27 @@
+require("dotenv").config({ path: './shared/config/.env' })
+const {ethers} = require("hardhat")
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.provider)
+
+async function main() {
+    const researchContract = await ethers.getContractFactory("Research");
+    const research = await researchContract.attach("0x8444933B3BEADc684146215BBF20fE5fbE785F51")
+
+    const labCoinContract = await ethers.getContractFactory("Labcoin");
+    const labCoin = await labCoinContract.attach("0xb8c468321368fe8b367914d656b724e31CEe2708");
+    const labcoin = await labCoin.connect(wallet)
+
+    // researchers will allow the contract to distribute the labcoins on their behalf.
+    const tx = await labcoin.approve(research.target, ethers.parseEther("100"));
+    await tx.wait()
+
+    console.log("Transaction: ", tx.hash);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
